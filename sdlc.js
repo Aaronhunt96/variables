45c5491ea2071f0f80cff7f92645656245e8fde3
Aaron hunt <117958720+Aaronhunt96@users.noreply.github.com>
2022-11-20 17:39:57 -0500

    Add files via upload
    added variables

---
+++ sdlc.js
@@ -0,0 +1,35 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Implementation
+// Design
+// Maintain
+// Analysis of Requirements
+// Testing & Integration
+// Planning
+
+//planning
+//design
+//developing
+//testing
+//maintenance
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+
+const design = "During the design phase, lead developers and technical architects create the initial high-level design plan for the software and system. This includes the delivery of requirements used to create the Design Document Specification (DDS). This document details database tables to be added, new transactions to be defined, security processes, as well as hardware and system requirements."
+
+const developing = "In this phase, the database admin creates and imports the necessary data into the database. Programming languages are defined by requirements. Developers create the interface as per the coding guidelines and conduct unit testing. This is an important phase for developers. They need to be open-minded and flexible if any changes are introduced by the business analyst."
+
+const testing = "Testers test the software against the requirements to make sure that the software is solving the needs addressed and outlined during the planning phase. All tests are conducted as functional testing, including unit testing, integration testing, system testing, acceptance testing, and non-functional testing."
+
+const maintenance = "In a post-production, live software environment, the system is in maintenance mode. No matter the number of users, the sophistication of the software and rigorous QA testing, issues will occur. That’s the nature of software with managing data, integration, and security, and real world usage. Access to knowledgeable, reliable support resources is essential, as is routine maintenance and staying up to date on upgrades."
